5bf7819a6a29498d894555943e8206946f4f31a3
Lily Ebadi <lilly888_ebadi@yahoo.com>
2024-10-15 20:56:44 -0500

    added stuff to footer

--- test/src/components/Footer.jsx
+++ test/src/components/Footer.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      
+    <a href="https://www.linkedin.com/in/lily-ebadi-75a792101/"> Lily Ebadi LinkedIn</a>
+    <a href="https://github.com/EbLily"> Lily Ebadi Github</a>
+      <p>&copy; 2024 lilyebadi.com</p>
+      </div>
   )
 }
 
